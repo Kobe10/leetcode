ee614339eedf3275779ce4ee50767fa85c91d142
fuzq <fuzq@ziroom.com>
2021-07-19 20:11:41 +0800

    7.19   字符串多种解法

---
+++ src/main/java/高频题/最长的有效括号.java
@@ -0,0 +1,83 @@
+package 高频题;
+
+/**
+ * <p></p>
+ * <p> 最长的有效括号 : https://leetcode-cn.com/problems/longest-valid-parentheses/solution/zui-chang-you-xiao-gua-hao-by-leetcode-solution/
+ * 给你一个只包含 '('和 ')'的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "(()"
+ * 输出：2
+ * 解释：最长有效括号子串是 "()"
+ * 示例 2：
+ * <p>
+ * 输入：s = ")()())"
+ * 输出：4
+ * 解释：最长有效括号子串是 "()()"
+ * 示例 3：
+ * <p>
+ * 输入：s = ""
+ * 输出：0
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= s.length <= 3 * 104
+ * s[i] 为 '(' 或 ')'
+ * <PRE>
+ * <BR>    修改记录
+ * <BR>-----------------------------------------------
+ * <BR>    修改日期         修改人          修改内容
+ * </PRE>
+ *
+ * @author fuzq
+ * @version 1.0
+ * @date 2021年07月19日 4:39 下午
+ * @since 1.0
+ */
+public class 最长的有效括号 {
+    // 正向逆向思维解法
+    public static int longestValidParentheses(String s) {
+        // 正向遍历  遇见( left+1  遇见) right+1  最大值max实时计算
+        int left = 0, right = 0;
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            }
+            if (s.charAt(i) == ')') {
+                right++;
+            }
+            // 当相等是  表示符合条件
+            if (left == right) {
+                result = Math.max(result, right * 2);
+            } else if (right > left) {
+                left = right = 0;
+            }
+        }
+
+        left = 0;
+        right = 0;
+        // 逆向遍历 遇见) right+1  遇见( left+1  最大值max实时计算
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ')') {
+                left++;
+            }
+            if (s.charAt(i) == '(') {
+                right++;
+            }
+            // 当相等是  表示符合条件
+            if (left == right) {
+                result = Math.max(result, (right) * 2);
+            } else if (right > left) {
+                left = right = 0;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "(()";
+        System.out.println(longestValidParentheses(s));
+    }
+}
---
+++ src/main/java/高频题/有效的括号.java
@@ -0,0 +1,84 @@
+package 高频题;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * <p></p>
+ * <p> 有效的括号
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
+ * <p>
+ * 有效字符串需满足：
+ * <p>
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 示例 1：
+ * <p>
+ * 输入：s = "()"
+ * 输出：true
+ * 示例2：
+ * <p>
+ * 输入：s = "()[]{}"
+ * 输出：true
+ * 示例3：
+ * <p>
+ * 输入：s = "(]"
+ * 输出：false
+ * 示例4：
+ * <p>
+ * 输入：s = "([)]"
+ * 输出：false
+ * 示例5：
+ * <p>
+ * 输入：s = "{[]}"
+ * 输出：true
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 104
+ * s 仅由括号 '()[]{}' 组成
+ * <PRE>
+ * <BR>    修改记录
+ * <BR>-----------------------------------------------
+ * <BR>    修改日期         修改人          修改内容
+ * </PRE>
+ *
+ * @author fuzq
+ * @version 1.0
+ * @date 2021年07月19日 4:31 下午
+ * @since 1.0
+ */
+
+public class 有效的括号 {
+    // 栈实现
+    public boolean isValid(String s) {
+        int n = s.length();
+        if (n % 2 == 1) {
+            return false;
+        }
+        // 闭括号做key  方便判断是否存在
+        Map<Character, Character> pairs = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+        Deque<Character> stack = new LinkedList<Character>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (pairs.containsKey(ch)) {
+                // 第一个不能为闭括号  栈顶元素必须和闭括号对应
+                if (stack.isEmpty() || !stack.peek().equals(pairs.get(ch))) {
+                    return false;
+                }
+                // 出栈
+                stack.pop();
+            } else {
+                // 入栈
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
